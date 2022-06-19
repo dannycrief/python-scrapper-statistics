69946e7b2369831cfee54ce06eedeca39fc870f0
dannycrief <step.kozbvb@gmail.com>
2022-06-19 19:58:35 +0200

    SCVisualAnalytics has been added

--- sc_statistics/sc_visual.py
+++ sc_statistics/sc_visual.py
@@ -11,7 +11,23 @@ class SCVisualAnalytics:
         self.app = Dash(__name__)
 
         self.app.layout = html.Div(children=[
-            html.H1(children='Hello Dash'),
+            html.H1(children='Welcome to SCVisualAnalytics'),
+
+            html.Div(children=[
+                dcc.Graph(
+                    id="mean_price",
+                    figure=self.__get_pie_mean_rental("price", "location", title="Mean price of retal"),
+                    style={'width': '50%'}
+                ),
+                dcc.Graph(
+                    id="mean_price_re",
+                    figure=self.__get_pie_mean_rental("deposit_price", "location", title='Mean deposit of retal'),
+                    style={'width': '50%'}
+                ),
+            ], style={
+                'display': 'flex',
+                'flex-direction': 'row'
+            }),
 
             dash_table.DataTable(
                 id='table',
@@ -24,7 +40,7 @@ class SCVisualAnalytics:
             ),
         ])
 
-    def run_server(self, debug: bool):
+    def run_server(self, debug: bool = False):
         self.app.run_server(debug=debug)
 
     def __get_table_columns(self):
@@ -33,5 +49,13 @@ class SCVisualAnalytics:
         for i in df_c.columns:
             if i in ['title', 'location', 'price', 'area', 'media_price', 'rooms_number', 'deposit_price']:
                 columns.append({"name": i, "id": i})
-
         return columns
+
+    def __get_mean_by_column(self, column):
+        return self.df.groupby(column).mean().reset_index()
+
+    def __get_pie_mean_rental(self, values, names, title):
+        df_m = self.__get_mean_by_column('location')
+        fig = px.pie(self.df, values=df_m[values], names=df_m[names], title=title, hole=.3)
+        fig.update_traces(textinfo='value')
+        return fig
